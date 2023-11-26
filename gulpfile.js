32e7044db82ea054d70e9a5aceebd475e6adbd66
Fondue <lukas@szsolutions.ch>
2023-11-26 13:16:19 +0100

    fix wrong upstream merge

--- gulpfile.js
+++ gulpfile.js
@@ -7,5 +7,10 @@ function copyIcons() {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
 	const nodeDestination = path.resolve('dist', 'nodes');
 
-	return src(nodeSource).pipe(dest(nodeDestination));
+	src(nodeSource).pipe(dest(nodeDestination));
+
+	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
+	const credDestination = path.resolve('dist', 'credentials');
+
+	return src(credSource).pipe(dest(credDestination));
 }
--- nodes/Flectra/Flectra.node.ts
+++ nodes/Flectra/Flectra.node.ts
@@ -1,8 +1,12 @@
 import { IExecuteFunctions } from 'n8n-core';
+import { OptionsWithUri } from 'request';
 
 import {
+	ICredentialsDecrypted,
+	ICredentialTestFunctions,
 	IDataObject,
 	ILoadOptionsFunctions,
+	INodeCredentialTestResult,
 	INodeExecutionData,
 	INodePropertyOptions,
 	INodeType,
@@ -241,6 +245,65 @@ export class Flectra implements INodeType {
 				}
 
 				return operations;
+			},
+		},
+		credentialTest: {
+			async flectraApiTest(
+				this: ICredentialTestFunctions,
+				credential: ICredentialsDecrypted,
+			): Promise<INodeCredentialTestResult> {
+				const credentials = credential.data;
+
+				try {
+					const body = {
+						jsonrpc: '2.0',
+						method: 'call',
+						params: {
+							service: 'common',
+							method: 'login',
+							args: [
+								flectraGetDBName(credentials?.db as string, credentials?.url as string),
+								credentials?.username,
+								credentials?.password,
+							],
+						},
+						id: Math.floor(Math.random() * 100),
+					};
+
+					const options: OptionsWithUri = {
+						headers: {
+							'User-Agent': 'n8n',
+							Connection: 'keep-alive',
+							Accept: '*/*',
+							'Content-Type': 'application/json',
+						},
+						method: 'POST',
+						body,
+						uri: `${(credentials?.url as string).replace(/\/$/, '')}/jsonrpc`,
+						json: true,
+					};
+					const result = await this.helpers.request!(options);
+					if (result.error || !result.result) {
+						return {
+							status: 'Error',
+							message: `Credentials are not valid`,
+						};
+					} else if (result.error) {
+						return {
+							status: 'Error',
+							message: `Credentials are not valid: ${result.error.data.message}`,
+						};
+					}
+				} catch (error) {
+					return {
+						status: 'Error',
+						message: `Settings are not valid: ${error}`,
+					};
+				}
+				return {
+					status: 'OK',
+					message: 'Authentication successful!',
+				};
 			},
 		},
 	};
